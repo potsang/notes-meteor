22e7b7361f0332c7d38cd0f8a3ab701210ebb61d
Potsang Huang <freelancer5858@yahoo.com>
2017-05-27 20:57:39 -0500

    setup page content layout

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -41,7 +41,7 @@ export class Editor extends React.Component {
   render() {
     if (this.props.note) {
       return (
-        <div>
+        <div className="editor">
           <input value={this.state.title} placeholder="Untitled note" onChange={this.handleTitleChange.bind(this)}/>
           <textarea value={this.state.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
           <button onClick={this.handleRemoval.bind(this)}>Delete Note</button>
@@ -49,9 +49,11 @@ export class Editor extends React.Component {
       );
     } else {
       return (
-        <p>
-          {this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}
-        </p>
+        <div className="editor">
+          <p>
+            {this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}
+          </p>
+        </div>
       );
     }
   }
